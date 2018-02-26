c99dd8111c41e51046bd57fda9e9b3b8f6d84cc6
ReaperCore7 <36156879+ReaperCore7@users.noreply.github.com>
2018-02-26 12:59:11 +0000

    Added Enemy & Damage

---
+++ Assets/Enemy1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour {
+
+    public float Health = 100;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        Debug.Log("Bang!!!");
+       
+        Player1 Mario = collision.collider.GetComponent<Player1>();
+        Mario.ApplyDamage(2);
+    }
+}
---
+++ Assets/Player1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+    public string logText = "Death to the Infadels";
+    public float speed = 2;
+    public float health = 100;
+
+    void Start () {
+       Debug.Log(logText);
+        ApplyDamage(0);
+	}
+	
+	
+	void Update () {
+        
+        Rigidbody2D Rigidbody = GetComponent<Rigidbody2D>();
+
+        Rigidbody.velocity = Vector2.right * speed;
+
+    }
+     public void ApplyDamage (float DealDamage)
+    {
+        health = health - DealDamage;
+    }
+}
